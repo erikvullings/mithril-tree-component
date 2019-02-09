6e8c5d9c2781dee70c9b41f5cac2ca5000008a4f
Erik Vullings <erik.vullings@gmail.com>
2019-02-09 12:35:16 +0100

    Removed unnecessary imports

--- packages/example/src/app.ts
+++ packages/example/src/app.ts
@@ -1,13 +1,8 @@
-import 'materialize-css/js/jquery.hammer';
-import 'materialize-css/js/hammer.min';
 import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min';
 import './styles.css';
 import m, { RouteDefs } from 'mithril';
 import { TreeView } from './components/tree-view';
 import { Layout } from './components/layout';
-
-export const M: { updateTextFields: () => void } = (window as any).Materialize;
 
 const routingTable: RouteDefs = {
   '/': {
