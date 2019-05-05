f4b4201fbd7115f49ae437a87011f15756f5d565
Erik Vullings <erik.vullings@gmail.com>
2019-05-05 12:21:29 +0200

    Use rollup for build

---
+++ packages/mithril-tree-component/rollup.config.js
@@ -0,0 +1,60 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import json from 'rollup-plugin-json';
+import { terser } from 'rollup-plugin-terser';
+
+const pkg = require('./package.json');
+
+export default {
+  input: `src/index.ts`,
+  watch: 'src/**',
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: pkg.main,
+      format: 'iife',
+      name: 'TreeContainer',
+      sourcemap: true,
+      globals: {
+        mithril: 'm',
+      },
+    },
+  ],
+  // Indicate here external modules you don't want to include in your bundle
+  external: ['mithril'],
+  // external: [...Object.keys(pkg.dependencies || {})],
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    // Allow json resolution
+    json(),
+    postcss(),
+    // Compile TypeScript files
+    typescript({
+      rollupCommonJSResolveHack: true,
+      typescript: require('typescript'),
+    }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules',
+      },
+    }),
+    // Resolve source maps to the original source
+    sourceMaps(),
+    // minifies generated bundles
+    terser(),
+  ],
+};
